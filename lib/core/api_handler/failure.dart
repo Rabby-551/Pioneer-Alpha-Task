4d90c13ec6753015ea99152938d2572d9a6424c8
Rabby-551 <rabby.raziul@gmail.com>
2025-12-16 02:17:36 +0600

    failor message

--- lib/core/api_handler/failure.dart
+++ lib/core/api_handler/failure.dart
@@ -1,8 +1,6 @@
 enum Failure {dioFailure, socketFailure, authFailure, severFailure, firebaseFailure, unknownFailure, outOfMemoryError, noData, timeout, forbidden}
 class DataCRUDFailure {
   final Failure failure;
-  /// Message to be shown to the user
-  /// Defaults to `fullError`
   final String uiMessage;
   final String fullError;
 
