848f6110fec37e711853099f1bd514f394815326
Rabby-551 <rabby.raziul@gmail.com>
2025-12-16 11:23:52 +0600

    create binary render reader form response model

--- lib/core/hive/adapters/github_repository_adapter.dart
+++ lib/core/hive/adapters/github_repository_adapter.dart
@@ -3,8 +3,31 @@ import 'package:pioneer_alpha_ltd_task/feature/home/model/github_repo_response_m
 
 class GitHubRepoAdapter extends TypeAdapter<GitHubRepoResponseModel> {
   @override
+  final int typeId = 0;
+
+  @override
   GitHubRepoResponseModel read(BinaryReader reader) {
-    throw UnimplementedError();
+    final id = reader.readInt();
+    final name = reader.readString();
+    final fullName = reader.readString();
+    final description = reader.readString();
+    final stargazersCount = reader.readInt();
+    final forksCount = reader.readInt();
+    final updatedAt = DateTime.fromMillisecondsSinceEpoch(reader.readInt());
+    final htmlUrl = reader.readString();
+    final owner = reader.read() as RepoOwner;
+    return GitHubRepoResponseModel(
+      id: id,
+      name: name,
+      fullName: fullName,
+      description: description,
+      stargazersCount: stargazersCount,
+      forksCount: forksCount,
+      updatedAt: updatedAt,
+      htmlUrl: htmlUrl,
+      owner: owner,
+    );
+    
   }
 
   @override
